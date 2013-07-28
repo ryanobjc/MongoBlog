6a214598ccef738014d602e41f6bfb7a627612ef
Haroun Serang <haroun@schematiclabs.com>
2013-07-28 16:16:21 -0700

    [CHORE] Initial Working Commit

---
+++ test/models/about.js
@@ -0,0 +1,12 @@
+var assert = require('assert')
+  , tests
+  , About = geddy.model.About;
+
+tests = {
+  'test stub, replace with your own passing test': function () {
+    assert.equal(true, false);
+  }
+
+};
+
+module.exports = tests;
