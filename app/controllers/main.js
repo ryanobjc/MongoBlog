672f19d234572e494cd0d74d0cefce1aa4b00587
Ryan Rawson <ryanobjc@gmail.com>
2013-08-14 12:17:55 -0700

    add back in the current page for 'home'

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -4,22 +4,13 @@ var strategies = require('../helpers/passport/strategies')
 var Main = function () {
 
   this.index = function (req, resp, params) {
-    var self = this
-      , User = geddy.model.User;
-    User.first({id: this.session.get('userId')}, function (err, user) {
-      var data = {
-        user: null
-      , authType: null
-      };
-      if (user) {
-        data.user = user;
-        data.authType = authTypes[self.session.get('authType')].name;
-      }
-      self.respond(data, {
+     var options = {
+       "activePage": "home"
+     }
+     this.respond({options: options, params: params}, {
         format: 'html'
       , template: 'app/views/main/index'
       });
-    });
   };
 
   this.login = function (req, resp, params) {
